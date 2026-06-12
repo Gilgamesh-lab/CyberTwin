4374caf0f212381007ba6cd79981019e9c51a5fa
Gilgamesh-lab <fabrice.amegadjen@gmail.com>
2026-06-12 12:11:33 +0200

    Ajout script

---
+++ script.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Entreprise(
+   id_entreprise INT,
+   nom VARCHAR(50),
+   secteur_activté VARCHAR(50),
+   nombre_employe INT,
+   nombre_de_serveur INT,
+   nombre_de_postes_clients INT,
+   actif TEXT,
+   service_expose TEXT,
+   gestion_des_vulnerabilite TEXT,
+   risque_cyber TEXT,
+   PRIMARY KEY(id_entreprise)
+);
